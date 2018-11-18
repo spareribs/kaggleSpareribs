6a5ff66e37ef4d88ab42c3e6cebd42a708518584
Spareribs <370835062@qq.com>
2018-11-18 21:15:53 +0800

    新增 特征处理 的一些方法

--- Overdue/ml/features/Readme.md
+++ Overdue/ml/features/Readme.md
@@ -2,24 +2,29 @@ features 主要是数据的预处理
 
 目前主要是简单的数据处理, 主要是模型上面的训练
 1. 需要 直接删除 的数据, 这些都是唯一的id标识, 会过拟合
-    - Unnamed: 0
-    - 用户ID
-    - trade_no：不知道是什么，可以分析下
-    - bank_card_no：卡号
-    - id_name：名字
-    - custid: ???
-    - 'source'：只有一个值xs，无意义
+    - bank_card_no：只有一个值 '卡号1' , 无区分度
+    - source：只有一个值 'xs' , 无区分度
+    - 'Unnamed: 0': 与预测值无关 
+    - custid: 与预测值无关 
+    - id_name：与预测值无关 
+    - trade_no：与预测值无关 
     
-2. 需要 离散化处理 的数据
+2. 【类别特征】需要 离散化处理 的数据
     - reg_preference_for_trad
+    - regional_mobility
+    - student_feature
+    - is_high_user
 
-3. 针对日期数据的处理(转换成年月日)
+3. 【日期】针对日期数据的处理(转换成年月日)
     - first_transaction_time
     - latest_query_time
     - loans_latest_time
 
-4. 缺失值的填充
+4. 【删除部分特征】：统计各个列标准差，将标准差小于0.1的特征剔除
+    
+5. 【缺失值】缺失值的填充
     - 目前只是简单的众数填充 TODO
+    - 缺失的数据作为一种新特征，衡量数据的完整度
 
-5. 归一化处理所有数据
+6. 归一化处理所有数据
     - 目前直接使用StandardScaler方法处理, 没搞明白 TODO
--- Overdue/ml/features/base.py
+++ Overdue/ml/features/base.py
@@ -55,7 +55,8 @@ print(train.describe().T)
 train = train.drop(["trade_no", "bank_card_no", "id_name", "Unnamed: 0", "custid", "source"], axis=1)
 print(train.shape)
 # 3.2 离散化处理
-train.groupby(['reg_preference_for_trad']).count()
+# 3.2.1 reg_preference_for_trad
+# train.groupby(['reg_preference_for_trad']).count()
 train["reg_preference_for_trad"] = train["reg_preference_for_trad"].replace("一线城市", "1")
 train["reg_preference_for_trad"] = train["reg_preference_for_trad"].replace("二线城市", "2")
 train["reg_preference_for_trad"] = train["reg_preference_for_trad"].replace("三线城市", "3")
@@ -68,15 +69,34 @@ train["reg_preference_for_trad"] = train["reg_preference_for_trad"].replace("境
 # 参考文章：https://blog.csdn.net/qq_36523839/article/details/80382924
 train = pd.get_dummies(train, columns=["reg_preference_for_trad"], prefix="reg_preference_for_trad")
 
+# 3.2.2 regional_mobility
+train = pd.get_dummies(train, columns=["regional_mobility"], prefix="regional_mobility")
+# 3.2.3 student_feature
+train['student_feature'].fillna(0, inplace=True)
+train = pd.get_dummies(train, columns=["student_feature"], prefix="student_feature")
+# 3.2.4 is_high_user
+train = pd.get_dummies(train, columns=["is_high_user"], prefix="is_high_user")
+print(train.columns)
+
 # 3.3 针对日期数据的处理(转换成年月日) - 后续处理，先直接删除 TODO
 train = train.drop(["first_transaction_time", "latest_query_time", "loans_latest_time"], axis=1)
 
-# 3.4 缺失值填充 - 先使用众数填充
+# 3.4 统计各个列标准差，将标准差小于0.1的特征剔除
+
+print("{0}".format("*" * 20))
+for feature in train.columns:  # 查看缺失的数据占比
+    train[feature] = train[feature].astype('float64')  # 将所有数据转换成 float64类型的数据
+    r = train[feature].std()
+    print("{0:.2f}% {1}".format(r, feature))
+    if r < 0.1:
+        train = train.drop([feature], axis=1)
+
+# 3.5 缺失值填充 - 先使用众数填充
 # train.info()  # 查看数据类型
-print("{0}\n     % freature".format("*" * 20))
+print("{0}\n      % freature".format("*" * 20))
 for feature in train.columns:  # 查看缺失的数据占比
     null_count = train[feature].isnull().sum()
-    print("{0:.4f} {1}".format(null_count * 100 / len(train), feature))
+    print("{0:.2f}% {1}".format(null_count * 100 / len(train), feature))
 
 train = train.fillna(0)  # 使用 0 替换所有 NaN 的值
 col = train.columns.tolist()[1:]
@@ -90,7 +110,6 @@ def missing(df, columns):
     col = columns
     for i in col:
         df[i].fillna(df[i].mode()[0], inplace=True)
-        df[i] = df[i].astype('float64')  # 将所有数据转换成 float64类型的数据
 
 
 missing(train, col)
