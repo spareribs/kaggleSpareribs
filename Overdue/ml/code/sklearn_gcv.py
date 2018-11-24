628e8714113dc8907edf691fc35e93f8f36540ef
Spareribs <370835062@qq.com>
2018-11-24 14:40:52 +0800

    添加各个模型的使用例子

--- Overdue/ml/code/sklearn_gcv.py
+++ Overdue/ml/code/sklearn_gcv.py
@@ -23,23 +23,86 @@ data_fp = open(features_path, 'rb')
 x_train, y_train = pickle.load(data_fp)
 data_fp.close()
 
-"""划分训练集和验证集，验证集比例为test_size"""
-if status_vali:
-    x_train, x_vali, y_train, y_vali = train_test_split(x_train, y_train, test_size=0.3, random_state=0)
-
 """=====================================================================================================================
-2 训练分类器, clf_name选择需要的分类器
+2 逻辑回归 - 使用网络搜索获得最优的参数
 """
 clf_name = "lr"
 clf = clfs[clf_name]
 
-"""=====================================================================================================================
-3 使用网络搜索获得最优的参数
-"""
-
 param_grid = {
     'C': [0.05, 0.1, 0.5, 1.5],
     'penalty': ['l1', 'l2']
+}
+
+grid = GridSearchCV(clf, param_grid, scoring='f1_micro')
+grid.fit(x_train, y_train)
+
+print("最优参数：{0}".format(grid.best_params_))
+print("最好的分数{0}".format(grid.best_score_))
+
+"""=====================================================================================================================
+3 SVM - 使用网络搜索获得最优的参数
+"""
+clf_name = "svm"
+clf = clfs[clf_name]
+
+param_grid = {
+    'C': [0.05, 0.1, 0.5, 1.5],
+    'penalty': ['l2'],
+    'dual': [True]
+}
+
+grid = GridSearchCV(clf, param_grid, scoring='f1_micro')
+grid.fit(x_train, y_train)
+
+print("最优参数：{0}".format(grid.best_params_))
+print("最好的分数{0}".format(grid.best_score_))
+
+"""=====================================================================================================================
+4 随机森林 - 使用网络搜索获得最优的参数
+"""
+clf_name = "rf"
+clf = clfs[clf_name]
+
+param_grid = {
+    'criterion': ['gini'],
+    'n_estimators': range(10, 71, 10)
+}
+
+grid = GridSearchCV(clf, param_grid, scoring='f1_micro')
+grid.fit(x_train, y_train)
+
+print("最优参数：{0}".format(grid.best_params_))
+print("最好的分数{0}".format(grid.best_score_))
+
+
+"""=====================================================================================================================
+5 xgboost - 使用网络搜索获得最优的参数
+"""
+clf_name = "xgb"
+clf = clfs[clf_name]
+
+param_grid = {
+    'max_depth': [3],
+    'learning_rate': [0.1],
+    'n_estimators': [50, 100, 500]
+}
+
+grid = GridSearchCV(clf, param_grid, scoring='f1_micro')
+grid.fit(x_train, y_train)
+
+print("最优参数：{0}".format(grid.best_params_))
+print("最好的分数{0}".format(grid.best_score_))
+
+"""=====================================================================================================================
+6 lightgbm - 使用网络搜索获得最优的参数
+"""
+clf_name = "lgb"
+clf = clfs[clf_name]
+
+param_grid = {
+    'boosting_type': ['gbdt'],
+    'n_estimators': [100, 200, 250]
 }
 
 grid = GridSearchCV(clf, param_grid, scoring='f1_micro')
